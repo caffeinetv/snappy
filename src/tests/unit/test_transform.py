2a2a3659edb90ac370da476ecc2d842dca2cb827
Joarley Moraes <joarleymoraes@gmail.com>
2017-10-02 11:36:43 -0300

    added auto=compress

--- src/tests/unit/test_transform.py
+++ src/tests/unit/test_transform.py
@@ -2,16 +2,56 @@ import unittest
 from transform import image_transform
 import PIL.Image
 import os
+from copy import copy
 
 BASE_DIR = 'tests/data'
 
 class ImageTranformTests(unittest.TestCase):
 
-	def test_resize(self):
-		filename = os.path.join(BASE_DIR, 'terminal.gif') # 35x28
-		operations = {'w': 50, 'h': 100}
-		output = image_transform(filename, operations)
-		img = PIL.Image.open(output)
-		self.assertEqual((operations['w'], operations['h']), img.size)
+    def test_resize(self):
+        filename = os.path.join(BASE_DIR, 'terminal.gif') # 35x28
+        operations = {'w': 50, 'h': 100}
+        output = image_transform(filename, operations)
+        img = PIL.Image.open(output)
+        self.assertEqual((operations['w'], operations['h']), img.size)
+
+    def test_resize_with_fit(self):
+        filename = os.path.join(BASE_DIR, 'terminal.gif') # 35x28
+        base_ops = {'w': 50, 'h': 100}
+        
+        operations = copy(base_ops)
+        operations.update({'fit': 'clip'})
+        output = image_transform(filename, operations)
+        img = PIL.Image.open(output)
+        self.assertEqual((operations['w'], operations['h']), img.size)
+
+        operations = copy(base_ops)
+        operations.update({'fit': 'crop'})
+        output = image_transform(filename, operations)
+        img = PIL.Image.open(output)
+        self.assertEqual((operations['w'], operations['h']), img.size)
+
+        #
+        # bounds will fit only one the dimensions
+        # in case the aspect ratio is different
+        #
+        operations = copy(base_ops)
+        operations.update({'fit': 'bounds'})
+        output = image_transform(filename, operations)
+        img = PIL.Image.open(output)
+        self.assertEqual((operations['w'], 40), img.size)
+
+    def test_compress(self):
+        filename = os.path.join(BASE_DIR, 'lincoln.jpg')
+        w, h = PIL.Image.open(filename).size
+        in_file_size = os.stat(filename).st_size
+        print(in_file_size)
+        operations = {'w': w, 'h': h, 'auto': 'compress'}
+        output = image_transform(filename, operations)
+        self.assertLess(os.stat(output).st_size, in_file_size)
+        print(os.stat(output).st_size)
+
+
+
 
 
--- src/transform.py
+++ src/transform.py
@@ -11,6 +11,7 @@ import boto3
 
 from snappy import response
 from snappy.settings import DEFAULT_FIT_BG_COLOR
+from snappy.utils import rnd_str
 
 
 # Set up the default logger
@@ -25,80 +26,88 @@ logging.getLogger('boto3').setLevel(logging.INFO)
 
 # TODO: remove try/except after setting local envs.
 try:
-	# Environment variable config
-	SERVERLESS_ENV = os.environ['SERVERLESS_ENV']
-	BUCKET = os.environ['BUCKET']
+    # Environment variable config
+    SERVERLESS_ENV = os.environ['SERVERLESS_ENV']
+    BUCKET = os.environ['BUCKET']
 except:
-	pass
+    pass
 
 
 TMP_DIR = '/tmp'
 
 def download_s3_obj(bucket, key):
-	pass
+    pass
 
 
 def get_obj_metadata(bucket, key):
-	pass
+    pass
 
 
 def image_transform(filename, operations):
-	"""
-	Transform the image specified by `filename` using the transformations specified by `operations`
-	Returns
+    """
+    Transform the image specified by `filename` using the transformations specified by `operations`
+    Returns
     -------
     str
         the filename of the transformed image
-	"""
-	basename = ntpath.basename(filename)
-	output = os.path.join(TMP_DIR, basename)
-	if 'w' in operations and 'h' in operations:
-		resize = (operations['w'], operations['h'])
-		new_size = '{}x{}'.format(*resize)
-		args = ['convert', filename, '-thumbnail', new_size]
-		if 'fit' in operations:
-			if operations['fit'] == 'clip':
-				pass
-			elif operations['fit'] == 'crop':
-				args[-1] += '^'
-				args.extend(['-background', DEFAULT_FIT_BG_COLOR, '-gravity', 'center', '-extent', new_size])
-			elif operations['fit'] == 'bounds':
-				args.extend(['-background', DEFAULT_FIT_BG_COLOR, '-gravity', 'center', '-extent', new_size])
-		else:
-			#
-			#  default behaviour is scale to the specificed bounds
-			#
-			args[-1] += '!'
-		#
-		# `thumbnail` already removes any image profile
-		# but not the ICC color profile by default
-		# so let's remove it as well with -strip
-		#
-		args.append('-strip')
+    """
+    basename = ntpath.basename(filename)
+    # TODO: FIXME: only for debugging
+    name, ext = basename.split('.')
+    if 'w' in operations and 'h' in operations:
+        resize = (operations['w'], operations['h'])
+        new_size = '{}x{}'.format(*resize)
+        args = ['convert', filename, '-thumbnail', new_size]
+        if 'fit' in operations:
+            # TODO: REMOVEME: only for debugging
+            name += '_' + operations['fit']
+            if operations['fit'] == 'clip':
+                args[-1] += '!'
+            elif operations['fit'] == 'crop':
+                args[-1] += '^'
+                args.extend(['-gravity', 'center', '-extent', new_size])
+            elif operations['fit'] == 'bounds':
+                pass
+        else:
+            #
+            #  default behaviour is to scale to the specificed bounds
+            #  which is the same behaviour as `fit=clip`
+            #
+            args[-1] += '!'
+        
 
-		args.append(output)
-		print ('args: {}'.format(args))
-		im_result = subprocess.check_output(args)
-		print (im_result.decode())
-	return output
+        if 'auto' in operations and operations['auto'] == 'compress':
+            #
+            # `thumbnail` already removes any image profile
+            # but not the ICC color profile by default
+            # so let's remove it as well with `-strip
+            #
+            args.append('-strip')
+
+        output = os.path.join(TMP_DIR, '{}_{}.{}'.format(name, rnd_str(5), ext))
+        args.append(output)
+        print ('args: {}'.format(args))
+        im_result = subprocess.check_output(args)
+        print (im_result.decode())
+    return output
 
 def param_validation(params):
-	"""
-	Validate the params and raise an Exception in case invalid or not supported operations
-	Returns
+    """
+    Validate the params and raise an Exception in case invalid or not supported operations
+    Returns
     -------
     dict
         the validated params
-	"""
-	pass
+    """
+    pass
 
 def make_response(image_data, s3_metadata):
-	pass
+    pass
 
 
 def http_transform(s3_key, query_params):
-	# image_data = transform()
-	pass
+    # image_data = transform()
+    pass
 
 
 def handler(event, context):
